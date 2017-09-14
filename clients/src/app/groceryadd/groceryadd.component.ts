18bc26ebf133fde9d40ea23988cae9102fafe236
royagasthyan@gmail.com <royagasthyan@gmail.com>
2017-09-14 23:10:07 +0530

    changes

---
+++ clients/src/app/groceryadd/groceryadd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'grocery-add',
+  templateUrl: './groceryadd.component.html',
+  styleUrls: ['./groceryadd.component.css']
+})
+export class GroceryAddComponent {
+  
+}
